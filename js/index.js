c49614ead7bf14d635f723f1199e742319b9f6e4
rosen777 <rosen.toshev@flatironschool.com>
2018-12-13 19:59:22 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 const app = "I don't do much.";
+
+let token = '963151f96710310f17169d7ae7c70284a6652dcf';
+
+fetch('https://api.github.com/user/repos', {
+    headers: {
+      Authorization: `token ${token}`
+    }
+  })
+  .then(res => res.json())
+  .then(json => console.log(json))
