1e7f7c12f8a02652473eb9fb7685f4f0a1f98717
artismarti <artismarti@gmail.com>
2018-12-13 16:28:20 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 const app = "I don't do much.";
+const token = '72effdefff25a18fe31829c27559a8ab8315df70'
+
+fetch('https://api.github.com/user/repos',{
+  headers: {
+    Authorization: `token ${token}`
+  }
+})
+  .then(res => res.json())
+  .then(json => console.log(json));
