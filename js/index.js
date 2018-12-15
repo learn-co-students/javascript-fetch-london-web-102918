ed55d5298a60dbaf81d416dfd586d3f9aacc75de
Chris <cwoolnoughchamorro@gmail.com>
2018-12-15 16:03:02 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 const app = "I don't do much.";
+
+
+fetch('https://api.github.com/user/repos')
+  .then(res => res.json())
+  .then(json => console.log(json));
+
+  const token = 'bb4b4e8864c832dc6a095b6ff16bde3b7ab5eace';
+  fetch('https://api.github.com/user/repos', {
+    headers: {
+      Authorization: `token ${token}`
+    }
+  })
+    .then(res => res.json())
+    .then(json => console.log(json));  
