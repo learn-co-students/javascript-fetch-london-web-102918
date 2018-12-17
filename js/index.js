d665f6b193a7e56348622aa821b602262017260c
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2018-12-17 11:06:39 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 const app = "I don't do much.";
+
+const token = 'e1d434c4d23bccd81094f8c8094e68c2f13a745d';
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+})
+  .then(res => res.json())
+  .then(json => console.log(json));
